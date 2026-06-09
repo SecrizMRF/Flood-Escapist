a9061d0b57a1e18ca0b0df9053393a4e700e8a63
edricrolandli <88447297+edricrolandli@users.noreply.github.com>
2026-06-09 21:47:39 +0700

    refactor script barrel

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Barrel : MonoBehaviour
+public class Barrel : Obstacle
 {
     private new Rigidbody2D rigidbody;
     public float speed = 1f;
@@ -11,11 +11,23 @@ public class Barrel : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    // Collision dengan ground (tidak menimpa OnCollisionEnter2D di base class)
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            Hit(player);
+            return;
+        }
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             rigidbody.AddForce(collision.transform.right * speed, ForceMode2D.Impulse);
         }
+    }
+
+    public override void OnHitPlayer(Player player)
+    {
+        GameManager.Instance.LevelFailed();
     }
 }
