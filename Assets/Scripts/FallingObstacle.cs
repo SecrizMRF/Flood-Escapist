a7b353f1e1002569988bafe9fd235d62a0cee7d8
SecrizMRF <88447297+edricrolandli@users.noreply.github.com>
2026-06-09 21:08:43 +0700

    Obstacle jatuh

---
+++ Assets/Scripts/FallingObstacle.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class FallingObstacle : MonoBehaviour
+{
+    [SerializeField] private float fallSpeed = 5f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.down * fallSpeed;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < -20f)
+            Destroy(gameObject);
+    }
+}
